671eff186128b8a56fd9e4ad4acab04628a08653
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2025-01-21 18:51:37 -0300

    prueba tecnica buscador peliculas en progreso

--- 05-react-buscador-peliculas/src/App.jsx
+++ 05-react-buscador-peliculas/src/App.jsx
@@ -1,63 +1,42 @@
 
-import { useState } from "react"
 import "./App.css"
 import { Movies } from "./components/Movies"
 import { useMovies } from "./hooks/useMovies"
-import { useEffect } from "react"
+import { useSearch } from "./hooks/useSearch"
 
-
-/* const SEARCH_URL = `http://www.omdbapi.com/?apikey=d6ad7582&`
- */
-
+/* const SEARCH_URL = `http://www.omdbapi.com/?apikey=d6ad7582&` */
 
 function App() {
  
   const {movies} = useMovies()
-  const [query,setQuery] = useState('')
-  const [error,setError] = useState(null)
+  const {search,updateSearch,error} = useSearch();
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(query)
+    console.log(search)
   }
 
   const handleChange = (event) => {
-    const newQuery = event.target.value
-    if(newQuery.startsWith(' ')) return
-    setQuery(newQuery)
+    updateSearch(event.target.value)
   }
-
-  useEffect(()=>{
-    if (query === '') {
-      setError('No se puede buscar una pelicula vacia')
-      return
-    }
-
-    if (query.match(/^\d+$/)){
-      setError('No se puede buscar una pelicula con un numero')
-      return
-    }
-
-    if(query.length<3){
-      setError('La busquede debe tener al menos 3 caracteres')
-      return
-    }
-
-    setError(null)
-  },[query])
-
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input onChange={handleChange} name="query" value={query} type="text" placeholder="Avengers, Star Wars, The matrix..."/>
+          <input 
+            style={{border:'1px solid transparent',borderColor: error ? 'red' : 'transparent' }} 
+            onChange={handleChange} 
+            name="query" 
+            value={search} 
+            type="text" 
+            placeholder="Avengers, Star Wars, The matrix..."/>
           <button type="submit">Buscar</button>
         </form>
         {
           error &&
-          <p>{error}</p>
+          <p style={{color:'red'}}>{error}</p>
         }
       </header>
      
---
+++ 05-react-buscador-peliculas/src/hooks/useSearch.js
@@ -0,0 +1,36 @@
+import { useEffect, useState , useRef} from "react"
+
+export function useSearch () {
+  const [search,updateSearch] = useState('')
+  const [error,setError] = useState(null)
+  const isFirstInput = useRef(true) // uso useRef ya que si uso
+                                    // un estado se actualiza
+                                    // el componente cada vez que
+                                    // escribo
+                                  
+  useEffect(()=>{
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una pelicula vacia')
+      return
+    }
+
+    if (search.match(/^\d+$/)){
+      setError('No se puede buscar una pelicula con un numero')
+      return
+    }
+
+    if(search.length<3){
+      setError('La busquede debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  },[search])
+  
+  return {search,updateSearch,error}
+}
